1fb2f4f6e3e8b0de0c5844ecd8b0fa9f31ff0e94
Aliaksandr Hrakhouski <aliaksandr_hrakhouski@epam.com>
2023-04-15 23:20:57 +0200

    done

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -1,5 +1,33 @@
 package com.example.filter;
 
-public class AuthFilter {
-    //write your code here!
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter(
+        urlPatterns = {"/user/*"}
+)
+public class AuthFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        if (req.getSession().getAttribute("user") == null){
+            req.getRequestDispatcher("/login.jsp").forward(servletRequest, servletResponse);
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
 }
--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,5 +1,43 @@
 package com.example.servlet;
 
-public class LoginServlet {
-    //write your code here!
+import com.example.Users;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+//have url /login.
+//check session attribute "user".
+//for GET request if the session attribute "user" does not exist, redirect to the /login.jsp page, else redirect to the /user/hello.jsp.
+//for POST request check the request parameters. "login" should exist in Users and the request parameter "password" shouldn't be empty.
+// If parameters are correct set session attribute "user" and redirect to /user/hello.jsp, else forward to the /login.jsp.
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        HttpSession session = req.getSession();
+        if(session.getAttribute("user") == null){
+            req.getRequestDispatcher("login.jsp").forward(req, resp);
+        }
+            req.getRequestDispatcher("user/hello.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userLogin = req.getParameter("login");
+        String userPassword = req.getParameter("password");
+
+        if(userPassword != null && userPassword.length() > 0 && Users.getInstance().getUsers().stream().anyMatch( u -> u.equals(userLogin))){
+            req.getSession().setAttribute("user", userLogin);
+            System.out.println("UserMatched");
+        }
+        doGet(req, resp);
+    }
 }
